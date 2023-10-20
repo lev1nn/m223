d128004121a673efe5c2989a9a66f379a45730a6
lev1nn <levinsch@bluewin.ch>
2023-10-20 13:27:31 +0200

    Create login & sign up

---
+++ test/controllers/user_session_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class UserSessionControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get user_session_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get user_session_create_url
+    assert_response :success
+  end
+end
