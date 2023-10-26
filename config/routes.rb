95c8c71c68032d4680f95b7b00e909c13a0addc8
lev1nn <levinsch@bluewin.ch>
2023-10-26 14:12:24 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#index'
+  get 'pages/index'
   get 'pages/secret'
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'pages/secret'
+  get 'users/index'
+  get 'users/new'
+  get 'users/create'
   resources :user_sessions, only: [:new, :create, :destroy]
   resources :users, only: [:index, :new, :create, :edit, :update, :destroy]
 end
