b30176a239756a15de082b7a373d4ba4c444a785
lev1nn <levinsch@bluewin.ch>
2023-10-27 09:05:38 +0200

    Add roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true, uniqueness: true
     validates :password, length: { minimum: 12 }
+    enum role: [
+        :user,
+        :admin
+    ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_20_083817) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_26_140053) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
