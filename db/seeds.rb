0ab980daf008cbc61a75ba58e65dc862482a5d50
lev1nn <levinsch@bluewin.ch>
2023-10-27 11:13:44 +0200

    Add test users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+# to import data into db -> bundle exec rails db:seed
+
+User.create(username: "admin", email: "admin@gmail.com", password_digest: "password1234", role: "admin")
+
+User.create(username: "Jay.H", email: "jaydon.hunt@outlook.com", password_digest: "password1234", role: "user")
+
+User.create(username: "MParker", email: "melanie.parker@gmx.com", password_digest: "password1234", role: "user")
+
+User.create(username: "John_Smith", email: "john.smith@gmail.com", password_digest: "password1234", role: "user")
+
+User.create(username: "Elsie_B", email: "elsie.ballard@yahoo.com", password_digest: "password1234", role: "user")
